91957fdefebd71e8419505ea2000d6416a600a16
magesharun <magesharun2016@gmail.com>
2025-08-02 22:02:37 +0530

    ss

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
-// Import Routes
 const productRoutes = require('./routes/productRoutes');
 const userRoutes = require('./routes/userRoutes');
 const orderRoutes = require('./routes/orderRoutes');
@@ -11,30 +10,25 @@ const analyticsRoutes = require('./analytics/summaryRoute');
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// MongoDB Connection
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-.then(() => console.log(' Connected to MongoDB'))
-.catch(err => console.error(' MongoDB connection error:', err));
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('MongoDB connection error:', err));
 
-// Base Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/analytics', analyticsRoutes);
 
-// Default Route
 app.get('/', (req, res) => {
-  res.send('🚀 E-Commerce Product Order & Analytics API Running');
+  res.send('E-Commerce Product Order & Analytics API Running');
 });
 
-// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(` Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
